7c90fe478b256fa867e8b195a3049549cf0a9b4b
Krimothiazine <krimouv1995@gmail.com>
2023-01-11 09:57:33 +0100

    Refactor to modular js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-lonely-if */
-const gameBoard = [];
+const gameBoardObj = (() => {
+  const gameBoard = [];
+
+  return { gameBoard };
+})();
 
 const Player = (choice, status) => {
   let canPlay = status;
@@ -19,14 +23,30 @@ const getDom = () => {
   const gameGridCells = Array.from(
     document.querySelectorAll('.game-grid-cell')
   );
-  return { choiceDivs, gameGridCells };
+  const winningMsg = document.querySelector('.winning-message');
+  const restartBtn = document.querySelector('#restart');
+  const oneX = document.querySelector('[data-choice="1-x"]');
+  const oneO = document.querySelector('[data-choice="1-o"]');
+  const twoX = document.querySelector('[data-choice="2-x"]');
+  const twoO = document.querySelector('[data-choice="2-o"]');
+  return {
+    choiceDivs,
+    gameGridCells,
+    winningMsg,
+    restartBtn,
+    oneX,
+    oneO,
+    twoX,
+    twoO,
+  };
 };
 
 const gameDom = getDom();
 
 const renderDom = () => {
   gameDom.gameGridCells.forEach((gameGridCell) => {
-    gameGridCell.textContent = gameBoard[gameGridCell.dataset.index];
+    gameGridCell.textContent =
+      gameBoardObj.gameBoard[gameGridCell.dataset.index];
   });
 };
 
@@ -43,28 +63,25 @@ const endGameRow = (index1, index2, index3) => {
 };
 
 const displayWinnerMsg = (winner) => {
-  const winningMsg = document.querySelector('.winning-message');
   if (winner === undefined) {
-    winningMsg.firstElementChild.textContent = 'Tie!';
+    gameDom.winningMsg.firstElementChild.textContent = 'Tie!';
   } else {
     if (winner === playerOne.choice) {
       winner = 'Player One';
     } else {
       winner = 'Player Two';
     }
-    winningMsg.firstElementChild.textContent = `${winner} wins`;
+    gameDom.winningMsg.firstElementChild.textContent = `${winner} wins`;
   }
   setTimeout(() => {
-    winningMsg.style.display = 'grid';
+    gameDom.winningMsg.style.display = 'grid';
   }, 1000);
 };
 
 const restartGame = () => {
-  const restartBtn = document.querySelector('#restart');
-  const winningMsg = document.querySelector('.winning-message');
-  restartBtn.addEventListener('click', () => {
-    winningMsg.style.display = 'none';
-    gameBoard.length = 0;
+  gameDom.restartBtn.addEventListener('click', () => {
+    gameDom.winningMsg.style.display = 'none';
+    gameBoardObj.gameBoard.length = 0;
     gameDom.gameGridCells.forEach((gameGridCell) => {
       gameGridCell.textContent = '';
       gameGridCell.style.backgroundColor = 'yellow';
@@ -77,62 +94,65 @@ const restartGame = () => {
 
 const declareResult = (playerMark) => {
   if (
-    gameBoard[0] === playerMark &&
-    gameBoard[1] === playerMark &&
-    gameBoard[2] === playerMark
+    gameBoardObj.gameBoard[0] === playerMark &&
+    gameBoardObj.gameBoard[1] === playerMark &&
+    gameBoardObj.gameBoard[2] === playerMark
   ) {
     endGameRow(0, 1, 2);
     displayWinnerMsg(playerMark);
   } else if (
-    gameBoard[3] === playerMark &&
-    gameBoard[4] === playerMark &&
-    gameBoard[5] === playerMark
+    gameBoardObj.gameBoard[3] === playerMark &&
+    gameBoardObj.gameBoard[4] === playerMark &&
+    gameBoardObj.gameBoard[5] === playerMark
   ) {
     endGameRow(3, 4, 5);
     displayWinnerMsg(playerMark);
   } else if (
-    gameBoard[6] === playerMark &&
-    gameBoard[7] === playerMark &&
-    gameBoard[8] === playerMark
+    gameBoardObj.gameBoard[6] === playerMark &&
+    gameBoardObj.gameBoard[7] === playerMark &&
+    gameBoardObj.gameBoard[8] === playerMark
   ) {
     endGameRow(6, 7, 8);
     displayWinnerMsg(playerMark);
   } else if (
-    gameBoard[0] === playerMark &&
-    gameBoard[3] === playerMark &&
-    gameBoard[6] === playerMark
+    gameBoardObj.gameBoard[0] === playerMark &&
+    gameBoardObj.gameBoard[3] === playerMark &&
+    gameBoardObj.gameBoard[6] === playerMark
   ) {
     endGameRow(0, 3, 6);
     displayWinnerMsg(playerMark);
   } else if (
-    gameBoard[1] === playerMark &&
-    gameBoard[4] === playerMark &&
-    gameBoard[7] === playerMark
+    gameBoardObj.gameBoard[1] === playerMark &&
+    gameBoardObj.gameBoard[4] === playerMark &&
+    gameBoardObj.gameBoard[7] === playerMark
   ) {
     endGameRow(1, 4, 7);
     displayWinnerMsg(playerMark);
   } else if (
-    gameBoard[2] === playerMark &&
-    gameBoard[5] === playerMark &&
-    gameBoard[8] === playerMark
+    gameBoardObj.gameBoard[2] === playerMark &&
+    gameBoardObj.gameBoard[5] === playerMark &&
+    gameBoardObj.gameBoard[8] === playerMark
   ) {
     endGameRow(2, 5, 8);
     displayWinnerMsg(playerMark);
   } else if (
-    gameBoard[0] === playerMark &&
-    gameBoard[4] === playerMark &&
-    gameBoard[8] === playerMark
+    gameBoardObj.gameBoard[0] === playerMark &&
+    gameBoardObj.gameBoard[4] === playerMark &&
+    gameBoardObj.gameBoard[8] === playerMark
   ) {
     endGameRow(0, 4, 8);
     displayWinnerMsg(playerMark);
   } else if (
-    gameBoard[2] === playerMark &&
-    gameBoard[4] === playerMark &&
-    gameBoard[6] === playerMark
+    gameBoardObj.gameBoard[2] === playerMark &&
+    gameBoardObj.gameBoard[4] === playerMark &&
+    gameBoardObj.gameBoard[6] === playerMark
   ) {
     endGameRow(2, 4, 6);
     displayWinnerMsg(playerMark);
-  } else if (!gameBoard.includes(undefined) && gameBoard.length === 9) {
+  } else if (
+    !gameBoardObj.gameBoard.includes(undefined) &&
+    gameBoardObj.gameBoard.length === 9
+  ) {
     displayWinnerMsg();
   }
 };
@@ -147,7 +167,6 @@ const setPlayerChoice = (target) => {
       playerOne.choice = 'O';
       playerTwo.choice = 'X';
     }
-    // target.style.backgroundColor = 'grey';
   } else {
     if (choice === '2-x') {
       playerOne.choice = 'O';
@@ -156,36 +175,30 @@ const setPlayerChoice = (target) => {
       playerOne.choice = 'X';
       playerTwo.choice = 'O';
     }
-    // target.style.backgroundColor = 'grey';
   }
 };
 
 const setChoiceBg = (target) => {
-  const oneX = document.querySelector('[data-choice="1-x"]');
-  const oneO = document.querySelector('[data-choice="1-o"]');
-  const twoX = document.querySelector('[data-choice="2-x"]');
-  const twoO = document.querySelector('[data-choice="2-o"]');
-
-  if (target === oneX) {
-    oneX.style.backgroundColor = 'grey';
-    oneO.style.backgroundColor = 'white';
-    twoX.style.backgroundColor = 'white';
-    twoO.style.backgroundColor = 'grey';
-  } else if (target === oneO) {
-    oneO.style.backgroundColor = 'grey';
-    oneX.style.backgroundColor = 'white';
-    twoX.style.backgroundColor = 'grey';
-    twoO.style.backgroundColor = 'white';
-  } else if (target === twoX) {
-    twoX.style.backgroundColor = 'grey';
-    twoO.style.backgroundColor = 'white';
-    oneX.style.backgroundColor = 'white';
-    oneO.style.backgroundColor = 'grey';
-  } else if (target === twoO) {
-    twoO.style.backgroundColor = 'grey';
-    twoX.style.backgroundColor = 'white';
-    oneX.style.backgroundColor = 'grey';
-    oneO.style.backgroundColor = 'white';
+  if (target === gameDom.oneX) {
+    gameDom.oneX.style.backgroundColor = 'grey';
+    gameDom.oneO.style.backgroundColor = 'white';
+    gameDom.twoX.style.backgroundColor = 'white';
+    gameDom.twoO.style.backgroundColor = 'grey';
+  } else if (target === gameDom.oneO) {
+    gameDom.oneO.style.backgroundColor = 'grey';
+    gameDom.oneX.style.backgroundColor = 'white';
+    gameDom.twoX.style.backgroundColor = 'grey';
+    gameDom.twoO.style.backgroundColor = 'white';
+  } else if (target === gameDom.twoX) {
+    gameDom.twoX.style.backgroundColor = 'grey';
+    gameDom.twoO.style.backgroundColor = 'white';
+    gameDom.oneX.style.backgroundColor = 'white';
+    gameDom.oneO.style.backgroundColor = 'grey';
+  } else if (target === gameDom.twoO) {
+    gameDom.twoO.style.backgroundColor = 'grey';
+    gameDom.twoX.style.backgroundColor = 'white';
+    gameDom.oneX.style.backgroundColor = 'grey';
+    gameDom.oneO.style.backgroundColor = 'white';
   }
 };
 
@@ -202,12 +215,12 @@ const addMark = (target) => {
     target.textContent !== playerTwo.choice
   ) {
     if (playerOne.canPlay) {
-      gameBoard[target.dataset.index] = playerOne.choice;
+      gameBoardObj.gameBoard[target.dataset.index] = playerOne.choice;
       playerOne.canPlay = false;
       playerTwo.canPlay = true;
       renderDom();
     } else {
-      gameBoard[target.dataset.index] = playerTwo.choice;
+      gameBoardObj.gameBoard[target.dataset.index] = playerTwo.choice;
       playerTwo.canPlay = false;
       playerOne.canPlay = true;
       renderDom();
@@ -226,3 +239,5 @@ gameDom.gameGridCells.forEach((gameGridCell) => {
 
 restartGame();
 renderDom();
+
+const DisplayController = (() => {})();
